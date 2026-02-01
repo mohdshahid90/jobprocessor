a7db5736fc076f3d5a9bf91e63f9715c519499f9
Mohd Shahid <shahidraj90.sr@gmail.com>
2026-02-01 20:53:06 +0530

    initial commit

---
+++ src/main/java/com/jobprocessor/jobprocessor/JobprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.jobprocessor.jobprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobprocessor/jobprocessor/JobprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobprocessor.jobprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
